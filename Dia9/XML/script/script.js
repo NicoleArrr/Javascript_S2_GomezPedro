24bf33c5dbbdabe384ea5c7ed7d3c5ee05d7beb1
Pedro Felipe Gomez Bonilla <pedro@gomez.com>
2025-07-31 11:00:12 -0500

    feat: :sparkles: Day 9 Part 2

--- Dia9/XML/script/script.js
+++ Dia9/XML/script/script.js
@@ -2,24 +2,47 @@
 // mediante XMLHttpRequest
 
 //Modularizar el consumo de la api
-function buscarPersonaje(){
+function buscarPersonaje() {
+    document.getElementById("resultados").innerHTML=``;
     //const nombreUsar= prompt("Ingresar el nombre del personaje a buscar:");
+    const nombreUsar = document.getElementById("nombreInput").value.trim();
+    console.log(nombreUsar);
     const xhr = new XMLHttpRequest();//La importación del XML
     const url = `https://rickandmortyapi.com/api/character?name=${nombreUsar}`;//Acento Inverso ```
     console.log(url);
-    xhr.open("GET",url,true);
-    xhr.onreadystatechange = function (){
-        if (xhr.readyState===3){
-            
-            //alert("Cargando...")
-        }
-        else if(xhr.readyState===4 && xhr.status === 200){
-            try{
+    xhr.open("GET", url, true);
 
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 3) {
+
+            console.log("Cargando...");
+        }
+        else if (xhr.readyState === 4 && xhr.status === 200) {
+            try {
+                /*        <div class="card">
+            <img src="https://rickandmortyapi.com/api/character/avatar/2.jpeg" alt="">
+            <h3>Morty</h3>
+            <p><strong>Estado:</strong>Vivo</p>
+            <p><strong>Especie:</strong>Humana</p>
+        </div> */
                 const daticos = JSON.parse(xhr.responseText);
-                //alert("The character is: Name:"+daticos["results"][0]["name"]+" Status:"+daticos["results"][0]["status"]+ " Specie:"+daticos["results"][0]["species"]);
+                if (daticos.results && daticos.results.length > 0) {
+                    for (let i = 0; i < daticos.results.length; i++) {
+                        let division = document.getElementById("resultados");
+                        division.innerHTML += `
+                        <div class="card">
+            <img src="${daticos["results"][i]["image"]}" alt="">
+            <h3>${daticos["results"][i]["name"]}</h3>
+            <p><strong>Status:</strong>${daticos["results"][i]["status"]}</p>
+            <p><strong>Specie:</strong>${daticos["results"][i]["species"]}</p>
+        </div>
+                        `
+                        console.log(daticos["results"][i]["name"]);
+                    }
+                }
+
             }
-            catch(err){
+            catch (err) {
                 console.log(err.message); //Si algo malo pasa simplemente imprima el error
             }
         }
@@ -27,5 +50,3 @@ function buscarPersonaje(){
     xhr.send();
 }
 
-
-buscarPersonaje();
