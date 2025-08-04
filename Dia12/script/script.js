7b3a6f6e5b9190630fd46432adb8f018497bd3d7
Pedro Felipe Gomez Bonilla <pedro@gomez.com>
2025-08-04 12:22:41 -0500

    feat: :sparkles: Day 12

---
+++ Dia12/script/script.js
@@ -0,0 +1,27 @@
+document.getElementById('btnColor').addEventListener('click',()=>{
+    console.log("Wenassssssss");
+    const cuadro = document.getElementById('cuadro');
+    cuadro.classList.toggle('cambio');
+    /*document.getElementById('cuadro').innerHTML+=`<h1>Wenas</h1>`;*/
+});
+
+//Elementos generales
+cuadro.addEventListener('click',()=>{
+    alert('Le diste click al cuadro!');
+});
+
+//mouseover y mouseout
+//hover (cuando esta encima de // cuando sale de ...)
+cuadro.addEventListener('mouseover',()=>{
+    cuadro.style.border='3px solid black';
+});
+cuadro.addEventListener('mouseout',()=>{
+    cuadro.style.border='none';
+});
+
+//Jugando con teclas
+document.addEventListener('keydown',function (e){
+    /*document.getElementById('textoXD').innerHTML+=`<p>Presionaste: ${e.key}</p>`;*/
+    console.log(`Presionaste: ${e.key}`);
+});
+
